4843b0ea29824f684195af1d30791faf7e19282b
Danielle Thompson <danithompson74@gmail.com>
2021-06-06 19:34:43 -0700

    Take out subscribe as per Steph instructions in mockups

--- js/components/_footer.js
+++ js/components/_footer.js
@@ -75,22 +75,10 @@ class Footer extends HTMLElement {
           <li class="nav-bar-menu"><a href="pages/merch.html">Merch ↗</a></li>
         </ul>
 
-        <form
-          action="mailto:strangebookingpdx@gmail.com"
-          style="padding-right: 40px; width: 300px;"
-          method="POST"
-          enctype="multipart/form-data"
-          name="EmailForm">
-          <fieldset>
-            <legend style="letter-spacing: 2px;">Get in Touch</legend>
-
-            <label for="email">
-              Email: <input type="text" id="email" name="email-address" placeholder="Let the magic happen." required/>
-            </label><br/>
-            <button class="btn btn-danger" style="text-align: center; width: 220px;" type="submit" value="Subscribe">SUBSCRIBE</button>
+        <div style="padding-right: 40px; width: 300px;">
+            <h4 style="letter-spacing: 2px;">Find Us</h4>
             <social-buttons-row></social-buttons-row>
-          </fieldset>
-        </form>
+        </div>
 
       </footer>
     `;
---
+++ js/components/_unused-subscribe-form.js
@@ -0,0 +1,16 @@
+{/* <form
+  action="mailto:strangebookingpdx@gmail.com"
+  style="padding-right: 40px; width: 300px;"
+  method="POST"
+  enctype="multipart/form-data"
+  name="EmailForm">
+  <fieldset>
+    <legend style="letter-spacing: 2px;">Find Us</legend>
+
+    <label for="email">
+      Email: <input type="text" id="email" name="email-address" placeholder="Let the magic happen." required/>
+    </label><br/>
+    <button class="btn btn-danger" style="text-align: center; width: 220px;" type="submit" value="Subscribe">SUBSCRIBE</button>
+    <social-buttons-row></social-buttons-row>
+  </fieldset>
+</form> */}
