f504437c48eced8257368c53923115095b35f0e7
Danielle Thompson <danithompson74@gmail.com>
2021-06-08 12:08:35 -0700

    Fix hamburger menu - stylilng still WIP

--- js/components/_header.js
+++ js/components/_header.js
@@ -6,117 +6,140 @@ class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <style>
-        body {
-          padding-top: 60px;
-          padding-bottom: 40px;
-        }
-
-        .fixed-header {
-          position: sticky;
-          top: 0;
-          width: 100%;
-          position: fixed;
-          padding: 10px 0;
-          color: #FFFEF2;
-          z-index: 1;
-        }
-
-        nav {
-          height: 75px;
-          position: fixed;
-          display: absolute;
-          text-align: center;
-          align-items: center;
-          padding-top: 15px;
-          background-color: black;
-          color: #FFFEF2;
-        }
-
-        nav a{
-          color: #fff;
-          text-decoration: none;
-          padding: 7px 25px;
-          display: inline-block;
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
       }
 
-        ul li {
-          list-style: none;
-          display: inline;
-          letter-spacing: 1px;
-        }
+      .fixed-header {
+        position: sticky;
+        top: 0;
+        width: 100%;
+        position: fixed;
+        padding: 10px 0;
+        color: #FFFEF2;
+        z-index: 1;
+      }
 
-        a {
-          font-weight: 700;
-          margin: 0 25px;
-          color: #fff;
-          text-decoration: none;
-          display: inline;
-        }
+      nav {
+        height: 75px;
+        position: fixed;
+        display: absolute;
+        text-align: center;
+        align-items: center;
+        padding-top: 15px;
+        background-color: black;
+        color: #FFFEF2;
+      }
 
-        a:hover {
-          color: #ff0000;
-          padding-bottom: 5px;
-          box-shadow: inset 0 -2px 0 0 red;
-        }
+      nav a{
+        color: #fff;
+        text-decoration: none;
+        padding: 7px 25px;
+        display: inline-block;
+    }
 
-        .container nav-container{
-          max-width: 90%
-          margin: 0 auto; /* Center the DIV horizontally */
-        }
+      ul li {
+        list-style: none;
+        display: inline;
+        letter-spacing: 1px;
+      }
 
-        #header-logo {
-          height: 50px;
-          width: auto;
-        }
+      a {
+        font-weight: 700;
+        margin: 0 25px;
+        color: #fff;
+        text-decoration: none;
+        display: inline;
+      }
 
-      </style>
+      a:hover {
+        color: #ff0000;
+        padding-bottom: 5px;
+        box-shadow: inset 0 -2px 0 0 red;
+      }
 
-      <div class="fixed-header">
-        <div class="nav-container">
-          <header>
-            <nav class="navbar fixed-top navbar-expand-lg">
-              <a class="nav-brand" href="/index.html"><img id="header-logo" src="../assets/images/StephanieStrange_StephAndNina_Logo_Solid.png"></a>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
+      .container nav-container{
+        max-width: 90%;
+        margin: 0 auto; /* Center the DIV horizontally */
+      }
 
-                  <button class="navbar-inverse navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                  </button>
+      #header-logo {
+        height: 50px;
+        width: auto;
+      }
 
-                  <li class="nav-item">
-                    <a class="nav-link" href="../../pages/about.html">About</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="../../pages/music.html">Music</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="../../pages/shows.html">Shows</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="../../pages/stories.html">Stories</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="../../pages/bookings.html">Bookings</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="btn btn-danger" href="pages/merch.html">Merch</a>
-                  </li>
+    </style>
 
-                </ul>
+    <div class="fixed-header">
+      <div class="nav-container">
+        <header>
+          <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+
+            <a class="nav-brand" href="/index.html"><img id="header-logo" src="../assets/images/StephanieStrange_StephAndNina_Logo_Solid.png"></a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+              <ul class="navbar-nav nav-pills flex-column flex-sm-row">
+
+                <li class="nav-item">
+                  <a class="nav-link" href="../../pages/about.html">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="../../pages/music.html">Music</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="../../pages/shows.html">Shows</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="../../pages/stories.html">Stories</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="../../pages/bookings.html">Bookings</a>
+                </li>
+                <li class="nav-item">
+                  <a class="btn btn-danger" href="https://strangeandthefamiliars.bandcamp.com/">Merch</a>
+                </li>
+
+              </ul>
+
+            </div>
+
+            <div class="pos-f-t">
+              <div class="collapse" id="navbarToggleExternalContent">
+                <div class="bg-dark p-4">
+                  <ul class="nav nav-pills flex-column flex-sm-row">
+
+                    <li class="nav-item">
+                      <a class="nav-link" href="../../pages/about.html">About</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="../../pages/music.html">Music</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="../../pages/shows.html">Shows</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="../../pages/stories.html">Stories</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="../../pages/bookings.html">Bookings</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="btn btn-danger" href="pages/merch.html">Merch</a>
+                    </li>
+
+                  </ul>
+                </div>
               </div>
-
-              <nav class="navbar navbar-dark">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-              </nav>
-
-          </header>
-        </div>
+            </div>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-custom navbar-toggler-icon"></span>
+              </button>
+        </header>
       </div>
+    </div>
     `;
   }
-
 }
 
 customElements.define('header-component', Header);
---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.navbar-toggler').toggle();
+});
+
+
+// <button class="navbar-inverse navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+{/* <span class="navbar-toggler-icon"></span>
+</button> */}
